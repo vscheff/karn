de8adda3cdad3a3503e8e757517232b67769af9f
vscheff <vonscheffler@protonmail.com>
2023-08-09 11:59:52 -0400

    Fixed missing price

--- Cogs/Cards.py
+++ Cogs/Cards.py
@@ -15,12 +15,14 @@ class Cards(commands.Cog):
 
         if "status" not in card_json:
             await ctx.send(card_json["image_uris"]["png"])
-            await ctx.send(f"**Price:** ${card_json['prices']['usd']}")
+            if price := card_json['prices']['usd']:
+                await ctx.send(f"**Price:** ${price}")
         elif "type" in card_json:
             complete_url = f"{BASE_URL}cards/search?q={search_name}"
             card_json = get_req(complete_url).json()
             for card_data in card_json["data"]:
                 await ctx.send(card_data["image_uris"]["png"])
-                await ctx.send(f"**Price:** ${card_data['prices']['usd']}")
+                if price := card_data['prices']['usd']:
+                    await ctx.send(f"**Price:** ${price}")
         else:
             await ctx.send(card_json["details"])
