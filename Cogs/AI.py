ca60cfe5258842e47166f02e5c62943bd4623d9c
vscheff <vonscheffler@protonmail.com>
2023-09-02 22:33:41 -0400

    $chat has channel-specific context

--- Cogs/AI.py
+++ Cogs/AI.py
@@ -1,4 +1,6 @@
+from discord import TextChannel
 from discord.ext.commands import Cog, command, MissingRequiredArgument
+from json import dump, load
 import openai
 from os import getenv
 from random import randint
@@ -7,6 +9,10 @@ from utils import package_message
 
 
 MIN_MESSAGE_LEN = 4
+MESSAGE_HISTORY_FILEPATH = "./msg_history.json"
+GENESIS_MESSAGE = {"role": "system",
+                   "content": "You are a time-travelling golem named Karn. "
+                              "You are currently acting as an AI assistant for a Discord server."}
 
 
 class AI(Cog):
@@ -15,18 +21,38 @@ class AI(Cog):
         openai.api_key = getenv("CHATGPT_TOKEN")
         openai.organization = getenv("CHATGPT_ORG")
 
-        self.messages = [{"role": "system", "content": "You are a Time-Travelling Golem named Karn."}]
+        try:
+            with open(MESSAGE_HISTORY_FILEPATH, 'r') as inFile:
+                self.messages = load(inFile)
+        except FileNotFoundError:
+            self.messages = {}
 
         self.reply_chance = 1
 
     @command(help="Generates natural language or code from a given prompt",
              brief="Generates natural language")
     async def chat(self, ctx, *, args):
-        self.messages.append({"role": "user", "content": args})
-        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.messages)
+        channel_id = str(ctx.id if isinstance(ctx, TextChannel) else ctx.channel.id)
+
+        if channel_id not in self.messages:
+            self.messages[channel_id] = [{key: val for key, val in GENESIS_MESSAGE.items()}]
+
+        self.messages[channel_id].append({"role": "user", "content": args})
+
+        while True:
+            try:
+                chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.messages[channel_id])
+            except openai.error.InvalidRequestError:
+                self.messages[channel_id].pop(1)
+            else:
+                break
+
         reply = chat.choices[0].message.content
         await package_message(reply, ctx)
-        self.messages.append({"role": "assistant", "content": reply})
+        self.messages[channel_id].append({"role": "assistant", "content": reply})
+
+        with open(MESSAGE_HISTORY_FILEPATH, 'w') as outFile:
+            dump(self.messages, outFile, indent=2)
 
     @chat.error
     async def chat_error(self, ctx, error):
@@ -39,7 +65,7 @@ class AI(Cog):
         if msg.author.id == bot_id or len(msg.content) < MIN_MESSAGE_LEN or msg.content[0] == '$':
             return
 
-        if "karn" in msg.content or "Karn" in msg.content:
+        if "karn" in msg.content.lower():
             return await self.chat(msg.channel, args=msg.content)
 
         if randint(1, 100) <= self.reply_chance:
