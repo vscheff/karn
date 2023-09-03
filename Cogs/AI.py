65c5e3b6d97146eb10e6aba61c29de3fadc1f01e
vscheff <vonscheffler@protonmail.com>
2023-09-03 14:29:35 -0400

    $prompt now uses SQL

--- Cogs/AI.py
+++ Cogs/AI.py
@@ -1,6 +1,7 @@
 from discord import TextChannel
 from discord.ext.commands import Cog, command, MissingRequiredArgument
 from json import dump, load
+from mysql.connector import connection, errors
 import openai
 from os import getenv
 from random import randint
@@ -21,38 +22,59 @@ class AI(Cog):
         openai.api_key = getenv("CHATGPT_TOKEN")
         openai.organization = getenv("CHATGPT_ORG")
 
+        conn_params = {"user": getenv("SQL_USER"),
+                       "password": getenv("SQL_PASSWORD"),
+                       "host": "localhost",
+                       "database": "discord"}
+
         try:
-            with open(MESSAGE_HISTORY_FILEPATH, 'r') as inFile:
-                self.messages = load(inFile)
-        except FileNotFoundError:
-            self.messages = {}
+            self.conn = connection.MySQLConnection(**conn_params)
+        except errors.ProgrammingError as e:
+            print(f"ERROR: Database connection failed with error:\n{e}.")
+            return
+
+        # try:
+        #     with open(MESSAGE_HISTORY_FILEPATH, 'r') as inFile:
+        #         self.messages = load(inFile)
+        # except FileNotFoundError:
+        #     self.messages = {}
 
         self.reply_chance = 1
 
     @command(help="Generates natural language or code from a given prompt",
-             brief="Generates natural language")
+             brief="Generates natural language",
+             aliases=["chat", "promt"])
     async def prompt(self, ctx, *, args):
-        channel_id = str(ctx.id if isinstance(ctx, TextChannel) else ctx.channel.id)
+        cursor = self.conn.cursor()
+        channel_id = ctx.id if isinstance(ctx, TextChannel) else ctx.channel.id
+        cursor.execute(f"SELECT usr_role, content, id FROM Karn WHERE channel_id = {channel_id}")
+        messages = [{"role": role, "content": content, "id": uuid} for role, content, uuid in cursor.fetchall()]
 
-        if channel_id not in self.messages:
-            self.messages[channel_id] = [{key: val for key, val in GENESIS_MESSAGE.items()}]
+        if not messages:
+            values = [channel_id, GENESIS_MESSAGE["role"], GENESIS_MESSAGE["content"]]
+            cursor.execute("INSERT INTO Karn (channel_id, usr_role, content, id) VALUES (%s, %s, %s, UUID())", values)
+            messages = [{key: val for key, val in GENESIS_MESSAGE.items()}]
 
-        self.messages[channel_id].append({"role": "user", "content": args})
+        messages.append({"role": "user", "content": args})
+        values = [channel_id, "user", args]
+        cursor.execute("INSERT INTO Karn (channel_id, usr_role, content, id) VALUES (%s, %s, %s, UUID())", values)
 
         while True:
             try:
-                chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.messages[channel_id])
+                context = [{key: val for key, val in i.items() if key != "id"} for i in messages]
+                chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=context)
             except openai.error.InvalidRequestError:
-                self.messages[channel_id].pop(1)
+                del_msg = messages.pop(1)
+                cursor.execute(f"DELETE FROM Karn WHERE id = {del_msg['id']}")
             else:
                 break
 
         reply = chat.choices[0].message.content
         await package_message(reply, ctx)
-        self.messages[channel_id].append({"role": "assistant", "content": reply})
-
-        with open(MESSAGE_HISTORY_FILEPATH, 'w') as outFile:
-            dump(self.messages, outFile, indent=2)
+        values = [channel_id, "assistant", reply]
+        cursor.execute("INSERT INTO Karn (channel_id, usr_role, content, id) VALUES (%s, %s, %s, UUID())", values)
+        self.conn.commit()
+        cursor.close()
 
     @prompt.error
     async def prompt_error(self, ctx, error):
