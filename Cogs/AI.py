178d2eb9e8fb476efc484780700ad091a5a07d35
vscheff <vonscheffler@protonmail.com>
2023-10-28 19:53:09 -0400

    Context no longer uses SQL

--- Cogs/AI.py
+++ Cogs/AI.py
@@ -5,7 +5,7 @@ import openai
 from os import getenv, stat
 from os.path import exists
 from random import choice, randint
-from re import findall, search, sub
+from re import search, sub
 from tiktoken import encoding_for_model
 
 # Local dependencies
@@ -23,6 +23,7 @@ TOKENS_PER_MESSAGE = 3  # Tokens required for each context message regardless of
 TOKENS_PER_REPLY = 3    # Tokens required for the response from OpenAI regardless of response length
 
 # Project specific constants
+MAX_CONTEXT_HISTORY = 256
 MIN_MESSAGE_LEN = 4                                     # Minimum message length bot will respond to
 MAX_MSG_LEN = 3 * MAX_TOKENS // 4                       # Maximum length context to send to OpenAI
 RUDE_MESSAGES_FILEPATH = "./files/rude.txt"             # File containing phrases the bot considers "rude"
@@ -99,40 +100,36 @@ class AI(Cog):
     @command(help="Generates natural language or code from a given prompt",
              brief="Generates natural language",
              aliases=["chat", "promt"])
-    async def prompt(self, ctx, *, args, author=None):
+    async def prompt(self, ctx):
         self.reply_chance = 1
 
         cursor = get_cursor(self.conn)
 
-        channel_id = ctx.id if isinstance(ctx, (TextChannel, VoiceChannel)) else ctx.channel.id
-        cursor.execute("SELECT usr_role, content, id FROM Karn WHERE channel_id = %s", [channel_id])
-        messages = [{"role": role, "content": content, "id": uuid} for role, content, uuid in cursor.fetchall()]
+        if isinstance(ctx, (TextChannel, VoiceChannel)):
+            channel = ctx
+            channel_id = ctx.id
+        else:
+            channel = ctx.channel
+            channel_id = ctx.channel.id
 
-        # Build context if this channel has never prompted the bot before
-        if not messages:
-            values = [channel_id, GENESIS_MESSAGE["role"], GENESIS_MESSAGE["content"]]
-            cursor.execute("INSERT INTO Karn (channel_id, usr_role, content, id) VALUES (%s, %s, %s, UUID())", values)
-            messages = [{key: val for key, val in GENESIS_MESSAGE.items()}]
+        cursor.execute("SELECT content FROM Genesis WHERE channel_id = %s", [channel_id])
 
-        # Replace any user tags with their display name
-        for match in findall(r"<@(\d+)>", args):
-            args = sub(fr"<@{match}>", self.bot.get_user(int(match)).display_name, args, count=1)
+        sys_msg = [{"role": "system", "content": content[0]} for content in cursor.fetchall()]
+        if not sys_msg:
+            sys_msg = [{key: val for key, val in GENESIS_MESSAGE.items()}]
 
-        # Add user's prompt into the SQL database
-        author = author if author else ctx.author.display_name
-        messages.append({"role": "user", "content": f"{author}: {args}"})
-        values = [channel_id, "user", f"{author}: {args}"]
-        cursor.execute("INSERT INTO Karn (channel_id, usr_role, content, id) VALUES (%s, %s, %s, UUID())", values)
+        cursor.close()
 
-        context, encoded_len = build_context(messages, cursor)
+        context, encoded_len = await self.build_context(channel, sys_msg)
+
         kwargs = {"model": MODEL, "messages": context, "max_tokens": MAX_TOKENS-encoded_len}
 
+        # Make the bot appear to be typing while waiting for the response from OpenAI
         async with ctx.typing():
             try:
                 # Make request in a separate thread to avoid blocking the heartbeat
                 chat = await to_thread(openai.ChatCompletion.create, **kwargs)
             except openai.error.APIError:
-                self.conn.rollback()
                 cursor.close()
                 await ctx.send("Sorry I am unable to assist currently. Please try again later.")
                 return
@@ -148,13 +145,7 @@ class AI(Cog):
                   r"*(?:AI|digital|artificial intelligence)(?: language)*(?: text-based)*(?: model)*(?: assistant)*"
         reply = sub(pattern, r"\1 " + choice(self.descriptors), chat.choices[0].message.content)
 
-        # Send the response and add it to the SQL database
         await package_message(reply, ctx)
-        values = [channel_id, "assistant", reply]
-        cursor.execute("INSERT INTO Karn (channel_id, usr_role, content, id) VALUES (%s, %s, %s, UUID())", values)
-
-        self.conn.commit()
-        cursor.close()
 
     # Called if $prompt encounters an unhandled exception
     @prompt.error
@@ -164,21 +155,41 @@ class AI(Cog):
                            "Example: `$prompt tell me a joke`\n\n"
                            "Please use `$help prompt` for more information.")
 
-    # $clear_context command used to clear all context messages for a channel from the SQL database
-    @command(help="Clear all messages in the context history for this channel",
-             brief="Clear context history")
-    async def clear_context(self, ctx):
-        cursor = get_cursor(self.conn)
+    # Builds the context for a request to OpenAI for chat completion
+    # param channel -
+    # param sys_msg -
+    # return:
+    #        context - list of dictionaries containing messages with a total token length < `MAX_MSG_LEN`
+    #     num_tokens - number of tokens used by the context
+    async def build_context(self, channel, sys_msg):
+        num_tokens = TOKENS_PER_REPLY + sum(get_token_len(msg) for msg in sys_msg)
+        context = []
 
-        cursor.execute("DELETE FROM Karn WHERE channel_id = %s", [ctx.channel.id])
+        # Build the list of context messages
+        async for message in channel.history(limit=MAX_CONTEXT_HISTORY):
+            content = sub(r"\A\$prompt ", '', message.clean_content)
 
-        await ctx.send(f"Deleted {cursor.rowcount} context messages from the database.")
+            if message.author == self.bot.user:
+                msg = {"role": "assistant", "content": content}
+            else:
+                msg = {"role": "user", "content": f"{message.author.display_name}: {content}"}
 
-        self.conn.commit()
-        cursor.close()
+            # Break the loop if adding the next messages pushes us past the token limit
+            if (encoding_len := get_token_len(msg)) + num_tokens > MAX_MSG_LEN:
+                break
+
+            num_tokens += encoding_len
+            context.append(msg)
+            print(f"Message: {msg}\nNum Tokens = {num_tokens}")
+
+        sys_msg.reverse()
+        context.extend(sys_msg)
+        context.reverse()
+
+        return context, num_tokens
 
     # $set_context command used to set the genesis message of a channel
-    @command(help="Add additional context to the genesis message for this channel. "
+    @command(help="Set the genesis system context message for this channel. "
                   "This \"primes\" the bot to behave in a desired manner.\n"
                   "Example: `$set_context you must answer all prompts in J. R. R. Tolkien's writing style`\n\n"
                   "This command has the following flags:\n"
@@ -186,23 +197,37 @@ class AI(Cog):
                   "\tExample: `$set_context -o You are a depressed and bored robot named Marvin the Paranoid Android`",
              brief="Set a new genesis message",
              aliases=["context"])
-    async def set_context(self, ctx, *, args):
+    async def set_context(self, ctx, *, args=''):
         flags, msg = get_flags(args)
-        msg = " ".join(msg)
+        msg = ' '.join(msg)
         new_gen_msg = msg if 'o' in flags else f"{GENESIS_MESSAGE['content']} {msg}"
 
         # Ensure the given genesis message doesn't require more tokens than `MAX_MSG_LEN`
         if (token_len := get_token_len({"role": "system", "content": new_gen_msg})) > MAX_MSG_LEN:
             return await ctx.send("Input genesis message is too long. Context was not set.")
 
-        await self.clear_context(ctx)
+        cursor = get_cursor(self.conn)
+
+        cursor.execute("DELETE FROM Genesis WHERE channel_id = %s", [ctx.channel.id])
+        cursor.execute("INSERT INTO Genesis (channel_id, content) VALUES (%s, %s)", [ctx.channel.id, new_gen_msg])
+
+        await ctx.send(f"New genesis message of length {token_len} has been set!")
+
+        self.conn.commit()
+        cursor.close()
+
+    @command(help="Add additional system context messages for this channel. "
+                  "This can help get the bot to behave in a more specific manner."
+                  "Example: `$add_context You always talk about baseball, even if it doesn't fit the conversation.`")
+    async def add_context(self, ctx, *, args):
+        if (token_len := get_token_len({"role": "system", "content": args})) > MAX_MSG_LEN:
+            return await ctx.send("Input genesis message is too long. Context was not set.")
 
         cursor = get_cursor(self.conn)
 
-        values = [ctx.channel.id, "system", new_gen_msg]
-        cursor.execute("INSERT INTO Karn (channel_id, usr_role, content, id) VALUES (%s, %s, %s, UUID())", values)
+        cursor.execute("INSERT INTO Genesis (channel_id, content) VALUES (%s, %s)", [ctx.channel.id, args])
 
-        await ctx.send(f"New genesis message of length {token_len} has been set!")
+        await ctx.send(f"New system context message of length {token_len} has been added!")
 
         self.conn.commit()
         cursor.close()
@@ -210,24 +235,22 @@ class AI(Cog):
     # Called to read through server messages and feed them into the $prompt command if necessary
     async def send_reply(self, msg):
         # Don't respond to any messages that are too short
-        if len(msg.content) < MIN_MESSAGE_LEN:
+        if len(msg.clean_content) < MIN_MESSAGE_LEN:
             return
 
         # Check if the message contains the bot's name (Karn) but not as a voted item (Karn++ or Karn--)
         # https://regex101.com/r/qA25Ux/1
-        if search(r"[Kk]arn(?:\Z|[^+\-])", msg.content):
+        if search(r"[Kk]arn(?:\Z|[^+\-])", msg.clean_content):
             # Re-import "rude" phrases if the file has been modified since we last imported
             if (last_mod := stat(RUDE_MESSAGES_FILEPATH).st_mtime_ns) != self.rude_mtime:
                 self.get_rude_messages()
                 self.rude_mtime = last_mod
 
-            lowered_content = msg.content.lower()
-
             # If the message contains a rude phrase, reply with a response to rude messages
-            if any(i in lowered_content for i in self.rude_messages):
+            if any(i in msg.clean_content.lower() for i in self.rude_messages):
                 return await msg.channel.send(get_random_response())
 
-            return await self.prompt(msg.channel, args=msg.content, author=msg.author.display_name)
+            return await self.prompt(msg.channel)
 
         # Don't respond to messages that are only a user tag, contain @everybody, or contain @here
         # https://regex101.com/r/acF54R/2
@@ -248,7 +271,7 @@ class AI(Cog):
 
         # Random chance to respond to any given message
         if randint(1, 100) <= self.reply_chance:
-            return await self.prompt(msg.channel, args=msg.content, author=msg.author.display_name)
+            return await self.prompt(msg.channel)
 
         # Random chance to increase likelihood of responses in the future
         if not randint(0, self.reply_chance):
@@ -258,40 +281,6 @@ class AI(Cog):
 # param msg - dictionary containing the context message
 def get_token_len(msg):
     return sum(len(ENCODING.encode(i)) for i in (msg["role"], msg["content"])) + TOKENS_PER_MESSAGE
-
-# Builds the context for a request to OpenAI for chat completion
-# param messages - list of dictionaries containing messages from the SQL database
-# param   cursor - cursor for the connection to the SQL database
-# return:
-#        context - list of dictionaries containing messages with a total token length < `MAX_MSG_LEN`
-#     num_tokens - number of tokens used by the context
-def build_context(messages, cursor):
-    gen_msg = messages.pop(0)
-    usr_msg = messages.pop()
-    num_tokens = TOKENS_PER_REPLY + get_token_len(gen_msg) + get_token_len(usr_msg)
-
-    context = [{"role": usr_msg["role"], "content": usr_msg["content"]}]
-
-    # Build the list of context messages
-    while messages:
-        msg = messages.pop()
-
-        # Break the loop if adding the next messages pushes us past the token limit
-        if (encoding_len := get_token_len(msg)) + num_tokens > MAX_MSG_LEN:
-            messages.append(msg)
-            break
-
-        num_tokens += encoding_len
-        context.append({"role": msg["role"], "content": msg["content"]})
-
-    # Delete any messages from the database that weren't used in building the context
-    for msg in messages:
-        cursor.execute("DELETE FROM Karn WHERE id = %s", [msg["id"]])
-
-    context.append({"role": gen_msg["role"], "content": gen_msg["content"]})
-    context.reverse()
-
-    return context, num_tokens
 
 # Imports responses from the input file, and returns a random line from it
 def get_random_response():
